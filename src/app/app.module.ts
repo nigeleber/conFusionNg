d61df0095d75629c030135665e7bb927774bd5f1
Nigel Eber <nigel.eber@hotmail.com>
2017-12-18 20:28:27 +0800

    HTTP Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,10 @@ import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+
+import { baseURL } from './shared/baseurl';
+import { ProcessHttpmsgService } from './services/process-httpmsg.service';
 
 @NgModule({
   declarations: [
@@ -45,12 +48,15 @@ import { LoginComponent } from './login/login.component'
     MaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [
     DishService,
     PromotionService,
-    LeaderService
+    LeaderService,
+    { provide: 'BaseURL', useValue: baseURL },
+    ProcessHttpmsgService
   ],
   entryComponents: [
     LoginComponent
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -52,11 +52,13 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishservice: DishService,
               private route: ActivatedRoute,
               private location: Location,
-              private fb: FormBuilder) {
-                this.createForm();
-  }
+              private fb: FormBuilder,
+              @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
+
+    this.createForm();
+
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
